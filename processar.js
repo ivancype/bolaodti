8f5cdbe4795f0dfe6298c3cbca3b591f5ab7b33a
ivancype <ivancype@hotmail.com>
2026-06-19 16:50:16 -0300

    Update processar.js

--- processar.js
+++ processar.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import { GoogleGenAI } from '@google/genai';
+import GoogleGenAI from '@google/genai';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
